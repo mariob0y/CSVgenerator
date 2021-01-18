52ac80800314fb926ad7161257306e3bdb5ad1da
mariob0y <69054485+mariob0y@users.noreply.github.com>
2021-01-18 17:34:53 +0200

    Update celery.py

--- planeks/celery.py
+++ planeks/celery.py
@@ -4,10 +4,16 @@ import os
 
 from celery import Celery
 
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planeks.settings')
 
 app = Celery('planeks')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 app.autodiscover_tasks()
