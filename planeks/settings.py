02ed36739b603a3847b792ee5047065974e1939c
mariob0y <69054485+mariob0y@users.noreply.github.com>
2021-01-18 18:49:36 +0200

    Update settings.py

--- planeks/settings.py
+++ planeks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,4 @@ MEDIAFILES_DIRS = [
     os.path.join(BASE_DIR, "media")
 ]
 
+django_heroku.settings(locals())
