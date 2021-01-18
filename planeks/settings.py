b29da3d8ea77673fec1ad4b79b39c2fb6d5f57b0
mariob0y <69054485+mariob0y@users.noreply.github.com>
2021-01-18 16:56:32 +0200

    Update settings.py

--- planeks/settings.py
+++ planeks/settings.py
@@ -127,9 +127,13 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 
