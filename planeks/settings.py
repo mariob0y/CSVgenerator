033a797090a8e731e81df9d325f6cda09146d028
mariob0y <69054485+mariob0y@users.noreply.github.com>
2021-01-18 18:09:58 +0200

    Update settings.py

--- planeks/settings.py
+++ planeks/settings.py
@@ -129,6 +129,7 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
