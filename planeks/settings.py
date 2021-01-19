23579e3e2ba1cd4e71065a02c70d58fea1e68ccf
mariob0y <69054485+mariob0y@users.noreply.github.com>
2021-01-19 12:23:02 +0200

    Update settings.py

--- planeks/settings.py
+++ planeks/settings.py
@@ -155,7 +155,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 AWS_QUERYSTRING_AUTH = True
 AWS_S3_REGION_NAME = 'eu-central-1'
-
+AWS_S3_ADDRESSING_STYLE = "virtual"
 
 MEDIA_URL = AWS_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
